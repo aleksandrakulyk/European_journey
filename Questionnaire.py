c60552345e0acb942fc8e74206a862d6e41be7c8
Aleksandra <143332018+aleksandrakulyk@users.noreply.github.com>
2024-07-30 18:43:17 +0200

    Create Questionnaire.py

---
+++ Questionnaire.py
@@ -0,0 +1,28 @@
+QUESTIONS = [
+    "Water or Mountains?: ",
+    "Seafood or Pasta? ",
+    "Hot or Cold? ",
+    "Camping or Apartment? ",
+    "Fast or Slow?"
+]
+
+ANSWERS_OPTIONS = [
+    'Water',
+    'Seafood',
+    'Hot',
+    'Apartment',
+    'Slow',
+    'Mountains',
+    'Pasta',
+    'Cold',
+    'Camping',
+    'Fast'
+]
+
+OPTIONS = {
+    'warm_countries_water': ['Croatia', 'Cyprus', 'Greece', 'Malta', 'Portugal', 'Hungary', 'Bulgaria', 'France',
+                             'Spain', 'Poland', 'Italy'],
+    'cold_countries_mountains': ['Austria', 'Chechia', 'Estonia', 'Romania', 'Slovakia', 'Poland', 'Norway', 'Belgium',
+                                 'Denmark', 'Finland', 'Ireland', 'Lithuania', 'Luxembourg', 'Latvia', 'Netherlands',
+                                 'Germany', 'Poland', 'Slovakia', 'Sweden']
+}
